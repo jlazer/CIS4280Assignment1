464db963629b0c9e718f003e790a2acabb55d2b1
Justin Lazarski <blackhawks189@gmail.com>
2021-02-25 22:07:15 -0800

    Implemented a plain textview to send to text over to the customer page. Also added in navigation back to parent activity.

--- app/src/main/java/com/Lazarski/cis4280assignment1/MainActivity.java
+++ app/src/main/java/com/Lazarski/cis4280assignment1/MainActivity.java
@@ -2,13 +2,47 @@ package com.Lazarski.cis4280assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.Lazarski.cis4280assignment1.MESSAGE";
+    private CheckBox toppingCheckbox1;
+    private CheckBox toppingCheckbox2;
+    private CheckBox toppingCheckbox3;
+    private CheckBox toppingCheckbox4;
+    private CheckBox toppingCheckbox5;
+    private CheckBox toppingCheckbox6;
+    private RadioGroup pizzaSizeRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        //Do something in response to button
+        Intent intent = new Intent(this, customerPage.class);
+        EditText editText =(EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
+
+
+    public void onCheckboxClicked(View view) {
+
     }
 }
--- app/src/main/java/com/Lazarski/cis4280assignment1/customerPage.java
+++ app/src/main/java/com/Lazarski/cis4280assignment1/customerPage.java
@@ -2,7 +2,9 @@ package com.Lazarski.cis4280assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class customerPage extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class customerPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_page);
+
+        // Get the Intent that Started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView4);
+        textView.setText(message);
     }
 }
