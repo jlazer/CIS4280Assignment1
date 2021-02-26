bc27a02abba5dbf5460c05f0ec9c2fe19cabb0b6
Justin Lazarski <blackhawks189@gmail.com>
2021-02-25 19:12:42 -0800

    Continued building out the mainActivity UI and created a customerPage activity.

---
+++ app/src/main/java/com/Lazarski/cis4280assignment1/customerPage.java
@@ -0,0 +1,14 @@
+package com.Lazarski.cis4280assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_page);
+    }
+}
